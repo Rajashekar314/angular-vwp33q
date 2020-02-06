fdbeb823926b0a240fd01735f78b650aa3cfc481
Rajashekar314 <rajashekar.duvvada@gmail.com>
2020-02-06 16:54:22 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-vwp33q
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-vwp33q)
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,26 @@
+import{ Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styles: [`
+        .online{
+          color: white;
+        }
+  `]
+})
+export class ServerComponent{
+serverId : number = 10;
+status : string= 'offline';
+
+constructor(){
+this.status = Math.random() > 0.5 ? 'online' : 'offline';
+}
+
+getServerStatus(){
+  return this.status;
+}
+getColour(){
+  return this.status === 'online' ? 'green' : 'red';
+}
+}
